5e42b853f7def0d9b5322931363b11ca344384ad
Giovana Almeida <162519099+giovanadealmeida@users.noreply.github.com>
2025-04-01 16:11:32 -0300

    inserindo-script
    inserção do script.sh e a explicao dos comandos utilizados.

---
+++ python_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#atualizar os pacotes
+sudo apt update
+#atulizar ou instalar o python
+sudo apt install python3
+
+#executar o programa
+python3 /home/giovana/modulo1/linux/tarefas_entregar/tarefa2/primeira_entrega.py
+
+
+
